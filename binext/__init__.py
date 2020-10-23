836631b932a7977f66999f047a5551bdfb1f8808
John M. Shea <jshea@ece.ufl.edu>
2020-10-23 14:21:23 -0400

    Restructure library so that things work properly (hopefully!)

---
+++ binext/__init__.py
@@ -0,0 +1,1 @@
+from .binext import ff, ffelt
--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="binext",
-    version="0.5",
-    scripts=['binext.py'],
+    version="0.61",
     author="John M. Shea",
     author_email="jshea@ece.ufl.edu",
     description="Opinionated module for working with binary Galois extension fields",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jmshea/coding/tree/main/binext",
-    packages=setuptools.find_packages(),
+    packages=['binext'],
     install_requires=[
           'numpy',
     ],
